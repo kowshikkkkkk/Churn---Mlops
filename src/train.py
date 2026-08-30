b966e67f7cd92d0bdd5f0836fcd9774f5b3c6a8c
Kowshik Sai S <kowshiksai14@gmail.com>
2026-08-30 20:43:42 +0530

    test: intentionally make model worse to test gates

--- src/train.py
+++ src/train.py
@@ -47,8 +47,9 @@ def train_model(data_path: str = "data/WA_Fn-UseC_-Telco-Customer-Churn.csv"):
 
         # Parameters
         n_estimators = 100
-        max_depth    = 4
+        max_depth    = 2
         class_weight = "balanced"
+        
 
         mlflow.log_param("n_estimators", n_estimators)
         mlflow.log_param("max_depth",    max_depth)
